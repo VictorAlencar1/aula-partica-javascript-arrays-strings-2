9f69bb76d248f1205cbf1fa60f11b47a8893cb46
Victor Alencar <87835966+VictorAlencar1@users.noreply.github.com>
2021-11-21 08:38:54 -0300

    Add files via upload

---
+++ atvd2.js
@@ -0,0 +1,32 @@
+function questao1(){
+    var tabuada = ["5 * 1", "5 * 2", "5 * 3", "5 * 4", "5 * 5", "5 * 6", "5 * 7", "5 * 8", "5 * 9", "5 * 10"];
+    console.log(tabuada.join(" ; "));
+  }
+  
+  function questao2(){
+    var formula = ["(n - 31) / 1.18"];
+    var temperatura1 = [54];
+    var resultado1 = [(54 - 31) / 1.18];
+    console.log(resultado1);
+    var temperatura2 = [78];
+    var resultado2 = [(78 - 31) / 1.18];
+    console.log(resultado2);
+    var temperatura3 = [56];
+    var resultado3 = [(56 - 31) / 1.18];
+    console.log(resultado3);
+    var temperatura4 = [98];
+    var resultado4 = [(98 - 31) / 1.18];
+    console.log(resultado4);
+    var temperatura5 = [0];
+    var resultado5 = [(0 - 31) / 1.18];
+    console.log(resultado5);
+    var temperatura6 = [12];
+    var resultado6 = [(12 - 31) / 1.18];
+    console.log(resultado6);
+    var temperatura7 = [11];
+    var resultado7 = [(11 - 31) / 1.18];
+    console.log(resultado7);
+    var temperatura8 = [37];
+    var resultado8 = [(37 - 31) / 1.18];
+    console.log(resultado8);
+  }
